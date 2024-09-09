5cb609925375bd53c0f0568038d0a57d54c55875
Shreyaa <sshreyaask@gmail.com>
2024-09-10 09:46:00 +1200

    Update CapacitiveTouchSensor.ino
    Comment code to make it easier to understand

--- CapacitiveTouch/CapacitiveTouchSensor.ino
+++ CapacitiveTouch/CapacitiveTouchSensor.ino
@@ -1,25 +1,32 @@
-#include <CapacitiveSensor.h>
-CapacitiveSensor capSensor = CapacitiveSensor(4,2);
-int threshold = 500;
-const int ledPinRed = 12;
+//use library manager in Arduino ide to download CapacitiveSensor by Paul Bagder & Paul Stoffregen
+#include <CapacitiveSensor.h> 
+
+CapacitiveSensor capSensor = CapacitiveSensor(4,2);// sensor input pin
+int threshold = 500; 
+// numbers of LED output pins
+const int ledPinRed = 12; 
 const int ledPinBlue = 7;
 
 void setup() {
-  // put your setup code here, to run once:
+  // starts serial monitor
   Serial.begin(9600);
+  // setting pin as output pin
   pinMode(ledPinRed,OUTPUT);
   pinMode(ledPinBlue,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // completes 30 runs before obtaining sensor value
   long sensorValue =  capSensor.capacitiveSensor(30);
+  // prints sensor value to serial monitor (can use this to make changes to threshold)
   Serial.println(sensorValue);
 
+  // if touched red light on
   if(sensorValue >threshold) {
     digitalWrite(ledPinRed,HIGH);
     digitalWrite(ledPinBlue,LOW);
   }
+  // if not touched blue light on
   else {
     digitalWrite(ledPinRed, LOW);
     digitalWrite(ledPinBlue,HIGH);
